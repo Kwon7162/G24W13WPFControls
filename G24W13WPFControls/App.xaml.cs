4a6625770a1ab60f60cb3717ddeedd0cbf76decc
권찬호 <rnjscksdud17@gmail.com>
2024-11-28 15:50:28 +0900

    초기 커밋.

---
+++ G24W13WPFControls/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace G24W13WPFControls
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
